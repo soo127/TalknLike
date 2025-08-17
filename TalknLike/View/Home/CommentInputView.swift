620e0aa51f6861153f0a3ce463f69ceada20f6fd
soo127 <ss_127@naver.com>
2025-08-18 00:22:17 +0900

    feat: CommentInputView / refactor: CommentCell,View를 View/Home 폴더로 이동

---
+++ TalknLike/View/Home/CommentInputView.swift
@@ -0,0 +1,63 @@
+//
+//  CommentInputView.swift
+//  TalknLike
+//
+//  Created by 이상수 on 8/18/25.
+//
+
+import UIKit
+
+final class CommentInputView: UIView {
+    
+    let profileImageView = UIImageView()
+    let textField = UITextField()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setup() {
+        setupProfileImageView()
+        setupTextField()
+        setupLayout()
+    }
+    
+}
+
+extension CommentInputView {
+    
+    private func setupProfileImageView() {
+        profileImageView.layer.cornerRadius = 18
+        profileImageView.clipsToBounds = true
+    }
+    
+    private func setupTextField() {
+        textField.placeholder = "댓글 추가..."
+        textField.borderStyle = .roundedRect
+    }
+    
+    private func setupLayout() {
+        addSubview(profileImageView)
+        addSubview(textField)
+        
+        profileImageView.anchor(
+            leading: leadingAnchor,
+            padding: UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 0),
+            width: 36,
+            height: 36,
+            centerY: centerYAnchor,
+        )
+        textField.anchor(
+            leading: profileImageView.trailingAnchor,
+            trailing: trailingAnchor,
+            padding: UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 8),
+            centerY: centerYAnchor,
+        )
+    }
+    
+}
