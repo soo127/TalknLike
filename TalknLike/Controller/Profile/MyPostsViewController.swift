da65b245dfcf73c95f98a7a5fdaa34b28475b38f
soo127 <ss_127@naver.com>
2025-08-28 13:53:57 +0900

    feat: '내 게시글' 에도 좋아요/댓글 가능하도록 추가, 게시글을 꾹 눌러 수정/삭제

--- TalknLike/Controller/Profile/MyPostsViewController.swift
+++ TalknLike/Controller/Profile/MyPostsViewController.swift
@@ -10,100 +10,99 @@ import FirebaseFirestore
 import Combine
 import FirebaseAuth
 
-final class MyPostsViewController: UIViewController {
+final class MyPostsViewController: BaseFeedViewController {
     
-    private let myPostsView = MyPostsView()
-    private var posts: [Post] = []
     private var cancellables = Set<AnyCancellable>()
-    
-    override func loadView() {
-        view = myPostsView
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "내 게시글"
-        setupTableView()
         bindPosts()
-    }
-    
-    private func setupTableView() {
-        myPostsView.tableView.dataSource = self
-        myPostsView.tableView.delegate = self
-        myPostsView.tableView.register(MyPostsCell.self, forCellReuseIdentifier: "MyPostsCell")
+        setupLongPressGesture()
     }
     
     private func bindPosts() {
         PostStore.shared.postsPublisher
             .receive(on: RunLoop.main)
             .sink { [weak self] posts in
-                self?.posts = posts
-                self?.myPostsView.tableView.reloadData()
+                guard let currentUser = CurrentUserStore.shared.currentUser else {
+                    return
+                }
+                let feedItems = posts.map { post in
+                    FeedItem(post: post, profile: currentUser)
+                }
+                self?.posts = feedItems
+                self?.tableView.reloadData()
             }
             .store(in: &cancellables)
     }
-
-}
-
-extension MyPostsViewController: UITableViewDataSource, UITableViewDelegate {
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return posts.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "MyPostsCell", for: indexPath) as? MyPostsCell,
-              let user = CurrentUserStore.shared.currentUser else {
-            return UITableViewCell()
-        }
-        let post = posts[indexPath.row]
-        cell.configure(title: post.title, nickname: user.nickname, post: post)
-        cell.delegate = self
-        Task { @MainActor in
-            cell.profileImage.image = await ImageLoader.loadImage(from: user.photoURL)
-        }
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
+    private func setupLongPressGesture() {
+        let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress(_:)))
+        longPressGesture.minimumPressDuration = 0.5
+        tableView.addGestureRecognizer(longPressGesture)
     }
     
 }
 
-extension MyPostsViewController: MyPostsCellDelegate {
+extension MyPostsViewController {
     
-    func didTapEdit(_ cell: MyPostsCell) {
-        guard let indexPath = myPostsView.tableView.indexPath(for: cell) else {
-            return
+    @objc private func handleLongPress(_ gesture: UILongPressGestureRecognizer) {
+        guard gesture.state == .began else { return }
+        
+        let point = gesture.location(in: tableView)
+        guard let indexPath = tableView.indexPathForRow(at: point) else { return }
+        
+        let post = posts[indexPath.row].post
+        showEditDeleteActionSheet(for: post, at: indexPath)
+    }
+    
+    private func showEditDeleteActionSheet(for post: Post, at indexPath: IndexPath) {
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        let editAction = UIAlertAction(title: "수정", style: .default) { [weak self] _ in
+            self?.editPost(post)
         }
-        let post = posts[indexPath.row]
+        
+        let deleteAction = UIAlertAction(title: "삭제", style: .destructive) { [weak self] _ in
+            self?.confirmDeletePost(post)
+        }
+        
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel)
+        
+        alert.addAction(editAction)
+        alert.addAction(deleteAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true)
+    }
+    
+    private func editPost(_ post: Post) {
         let nav = UINavigationController(rootViewController: PostViewController(mode: .edit(post)))
         nav.modalPresentationStyle = .fullScreen
         present(nav, animated: true)
     }
     
-    func didTapRemove(_ cell: MyPostsCell) {
-        guard let indexPath = myPostsView.tableView.indexPath(for: cell),
-              let documentID = posts[indexPath.row].documentID else {
-            return
-        }
+    private func confirmDeletePost(_ post: Post) {
+        guard let documentID = post.documentID else { return }
+        
         let alert = UIAlertController(
             title: "삭제",
             message: "정말 삭제하시겠습니까?",
             preferredStyle: .alert
         )
-        let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel)
         let confirmAction = UIAlertAction(title: "확인", style: .destructive) { [weak self] _ in
             Task {
                 try await PostStore.shared.deletePost(documentID: documentID)
                 self?.showToast(message: "게시글을 삭제했어요.")
             }
         }
+        
         alert.addAction(cancelAction)
         alert.addAction(confirmAction)
         present(alert, animated: true)
     }
     
 }
-
